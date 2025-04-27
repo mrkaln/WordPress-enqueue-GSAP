987c2096ac6068a75a985890c4d2694197b3cb10
Mark <mallenr@gmail.com>
2025-04-27 14:51:56 -0400

    Format code

--- gsap_wordpress.php
+++ gsap_wordpress.php
@@ -1,4 +1,5 @@
 <?php
+
 // The proper way to enqueue GSAP script in WordPress
 
 // wp_enqueue_script( $handle, $src, $deps, $ver, $in_footer );
@@ -11,4 +12,5 @@ function theme_gsap_script(){
 	  // Your animation code file - with gsap.js passed as a dependency
 	  wp_enqueue_script( 'gsap-js2', get_template_directory_uri() . 'js/app.js', array('gsap-js'), false, true );
   }
+  
   add_action( 'wp_enqueue_scripts', 'theme_gsap_script' );
